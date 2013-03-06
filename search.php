e1d32c38664b384583111fc9ba30525f5a79c5bd
Konurov Dmitry <dkonurov@s-zl.ru>
2013-03-06 13:43:33 +0400

    search 90%

--- search.php
+++ search.php
@@ -2,10 +2,32 @@
 error_reporting(E_ALL);
 
 ini_set('display_errors', 1);
+include 'header.php';
+include 'config.php';
 if ($_GET['search']){
-$search=(int)$_GET['search'];
-$query ="SELECT * FROM quotes WHERE (locate($search,id)>0)";
-$result = mysql_query($query);
+	if (!$_GET['page']){
+	$currpage = 1;
+	}
+	else{
+	$currpage = $_GET['page'];
+	}
+	$search=$_GET['search'];
+	$c = $currpage*20 - 20;
+	$query ="SELECT * FROM quotes WHERE msg or id LIKE '%$search%'";
+	$result = mysql_query($query);
+	if (mysql_num_rows($result)>20){
+		$query ="SELECT * FROM quotes WHERE msg or id LIKE '%$search%' LIMIT $c, 20";
+		$result = mysql_query($query);
+		$nextpage = $currpage+1;
+		$prevpage = $currpage-1;	
+
+		$c = $currpage*20 - 20;
+
+		echo "<br>";
+		echo "<a style=\"margin-left:600px;text-decoration:none;\" href=\"/parser/search.php?search=$search&page=$prevpage\"><</a>";
+		echo " "; echo $currpage; echo " ";
+		echo "<a style=\"text-decoration:none;\" href=\"/parser/search.php?search=$search&page=$nextpage\">></a>";
+	}
 if (mysql_num_rows($result) > 0) {
 	while ($row = mysql_fetch_assoc($result)) {
 		$id = $row['id'];
